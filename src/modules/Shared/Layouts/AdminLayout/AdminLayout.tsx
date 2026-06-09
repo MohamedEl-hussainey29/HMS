5e29b8429c52b6211621d15fb6adbe07472c7226
Mohamed Elhussainey <mw61887@gmail.com>
2026-06-09 21:40:40 +0300

    starting the adminLayout template

--- src/modules/Shared/Layouts/AdminLayout/AdminLayout.tsx
+++ src/modules/Shared/Layouts/AdminLayout/AdminLayout.tsx
@@ -1,19 +1,26 @@
-import Button from "@mui/material/Button";
-import { useContext } from "react";
-import { AuthContext } from "../../../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import NavBar from "../../NavBar/NavBar";
+import Grid from "@mui/material/Grid";
+import SideBar from "../../SideBar/SideBar";
+import { Outlet } from "react-router-dom";
+
 
 export default function AdminLayout() {
-    const navigate = useNavigate();
-    const authContext = useContext(AuthContext);
-    const logout = () => {
-      localStorage.removeItem("token");
-      authContext?.setUserData(null);
-      navigate("/auth");
-  };
+    
   return (
-    <div>
-      <Button variant="contained" size="large" onClick={logout}>logout</Button>
-    </div>
+    <>
+      <Box >
+        <Grid sx={{display:"flex" , overflow:"hidden" , height : 'calc(100vh - 70px)'}}>
+          <Box sx={{bgcolor:'blue',height: '100%', flexShrink: 0}}>
+            <SideBar/>
+          </Box>
+
+          <Box sx={{overflow:'auto'}} >
+            <NavBar/>
+            <Outlet/>
+          </Box>
+        </Grid>
+      </Box>
+    </>
   )
 }
