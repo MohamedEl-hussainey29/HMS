fb1184677dd798a9e1909127ef6885bda2133f15
Mohamed Elhussainey <mw61887@gmail.com>
2026-06-15 22:24:35 +0300

    adding Room Card and Bread Crumbs shared components

---
+++ src/modules/Shared/RoomCard/RoomCard.tsx
@@ -0,0 +1,115 @@
+import { Box, IconButton, Typography } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import { useNavigate } from "react-router-dom";
+
+export interface Room {
+  _id: string;
+  roomNumber: string;
+  price: number;
+  capacity: number;
+  discount: number;
+  facilities: { _id: string; name: string }[];
+  createdBy: { _id: string; userName: string };
+  images: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface RoomCardProps {
+  room: Room;
+}
+
+export default function RoomCard({ room }: RoomCardProps) {
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        borderRadius: 2,
+        overflow: "hidden",
+        border: "0",
+        position: "relative",
+        cursor: "pointer",
+        "&:hover .hover-overlay": { opacity: 1 },
+      }}
+    >
+      <Box
+        component="img"
+        src={room.images[0]}
+        alt={`Room ${room.roomNumber}`}
+        sx={{width: "100%", height: 220, objectFit: "cover", display: "block"}}
+      />
+
+      {/* Hover overlay */}
+      <Box
+        className="hover-overlay"
+        sx={{
+          position: "absolute",
+          inset: 0,
+          bgcolor: "rgba(0,0,0,0.3)",
+          opacity: 0,
+          transition: "opacity 0.25s ease",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 2,
+        }}
+      >
+        <IconButton
+          sx={{
+            color: "#FFF",
+            transition: "all 0.2s ease",
+          }}
+          onClick={()=> navigate(`/room-details/${room._id}`)}
+        >
+          <VisibilityOutlinedIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton
+          sx={{
+            color: "#FFF",
+            transition: "all 0.2s ease",
+          }}
+        >
+          <FavoriteIcon fontSize="large" />
+        </IconButton>
+      </Box>
+
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bgcolor: "#FF498B",
+          color: "#fff",
+          px: 3,
+          py: 1.5,
+          borderBottomLeftRadius: "15px",
+        }}
+      >
+        <Typography variant="body2" sx={{ fontWeight: 500 }}>
+          ${room.price} per night
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          px: 1.5,
+          py: 1,
+          background: "linear-gradient(to top, rgba(0,0,0,0.7), transparent)",
+        }}
+      >
+        <Typography variant="body1" sx={{ color: "#fff", fontWeight: 500 }}>
+          Room {room.roomNumber}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "rgba(255,255,255,0.85)" }}>
+          Capacity: {room.capacity}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
--- src/modules/User/LandingPage/components/Home.tsx
+++ src/modules/User/LandingPage/components/Home.tsx
@@ -1,6 +1,82 @@
+import {Grid} from "@mui/material";
+import RoomCard from "../../../Shared/RoomCard/RoomCard";
+import roomImage from "../../../../assets/images/old-money-style-bedroom@2x.jpg"
+
+export interface Room {
+  _id: string;
+  roomNumber: string;
+  price: number;
+  capacity: number;
+  discount: number;
+  facilities: { _id: string; name: string }[];
+  createdBy: { _id: string; userName: string };
+  images: string[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+
 
 export default function Home() {
+  const rooms = [
+  {
+    _id: "6a27123b74cb50ccc74c65fd",
+    roomNumber: "701",
+    price: 2500,
+    capacity: 6,
+    discount: 19,
+    facilities: [],
+    createdBy: { _id: "69f50dd984cd475a059952d5", userName: "bassant" },
+    images: [roomImage],
+    createdAt: "2026-06-08T19:04:27.185Z",
+    updatedAt: "2026-06-14T15:50:33.371Z",
+  },
+  {
+    _id: "6a27123b74cb50ccc74c65fd",
+    roomNumber: "702",
+    price: 1500,
+    capacity: 4,
+    discount: 8,
+    facilities: [],
+    createdBy: { _id: "69f50dd984cd475a059952d5", userName: "bassant" },
+    images: [roomImage],
+    createdAt: "2026-06-08T19:04:27.185Z",
+    updatedAt: "2026-06-14T15:50:33.371Z",
+  },
+  {
+    _id: "6a27123b74cb50ccc74c65fd",
+    roomNumber: "703",
+    price: 2000,
+    capacity: 2,
+    discount: 15,
+    facilities: [],
+    createdBy: { _id: "69f50dd984cd475a059952d5", userName: "bassant" },
+    images: [roomImage],
+    createdAt: "2026-06-08T19:04:27.185Z",
+    updatedAt: "2026-06-14T15:50:33.371Z",
+  },
+  {
+    _id: "6a27123b74cb50ccc74c65fd",
+    roomNumber: "704",
+    price: 1850,
+    capacity: 3,
+    discount: 12,
+    facilities: [],
+    createdBy: { _id: "69f50dd984cd475a059952d5", userName: "bassant" },
+    images: [roomImage],
+    createdAt: "2026-06-08T19:04:27.185Z",
+    updatedAt: "2026-06-14T15:50:33.371Z",
+  },
+];
   return (
-    <div>Home</div>
+    <>
+      <Grid container spacing={2}>
+        {rooms.map((room) => (
+          <Grid key={room.roomNumber} size={{ xs: 12, sm: 6, md: 3 }}>
+            <RoomCard room={room} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   )
 }
